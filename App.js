5c54ccf8c10201fe2f76c53e84362d8ea5b55588
Sachin <sachinkaintura2002@gmail.com>
2025-07-23 20:23:43 +0530

    adding other things

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const parent = React.createElement("div" , {id: "parent"}, React.createElement("div" , {id: "child"} , React.createElement(["h1", {}, "Welcome to my react nested code " , "h2", {}, "Welcome to my react nested code " ] )))
+import React from "react"
+import ReactDOM from "react-dom/client"
+const parent = React.createElement("div" , {id: "parent"}, React.createElement("div" , {id: "child"} , React.createElement("h1", {}, "Welcome to my react nested code "  )  ,React.createElement("h1", {}, "Welcome to my react nested code "  )))
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 .render(parent) 
